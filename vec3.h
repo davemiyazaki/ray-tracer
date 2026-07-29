5122e1211b227749d223e8723e38db9d4a8c2c6a
ミヤザキ <denixden2818@gmail.com>
2026-07-28 21:43:34 -0400

    add vec3 class with basic contructs

---
+++ vec3.h
@@ -0,0 +1,13 @@
+#ifndef VEC3H
+#define VEC3H
+
+#include <math.h>
+#include <stdlib.h> 
+#include <iostream>
+
+class vec3{
+
+    public:
+    vec3(){}
+    vec3(float e0, float e1, float e2){e[0] = e0; e[1] = e1; e[2] = e2; }
+};
