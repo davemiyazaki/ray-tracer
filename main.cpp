8bab063477b121d2fdc1b0f63f8f61300050eb94
ミヤザキ <denixden2818@gmail.com>
2026-07-29 22:44:49 -0400

    main.cpp: create loop to run across the image hxw

--- main.cpp
+++ main.cpp
@@ -11,5 +11,10 @@ int main(){
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
+    for(int j = 0; j < image_height; j++){
+        for(int i = 0; i < image_width; i++){
+            
+        }
+    }
     return 0;
 }
