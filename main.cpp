e35b0e06f8f2cdd071f333be98eaed114fcacfa3
ミヤザキ <denixden2818@gmail.com>
2026-07-29 22:41:32 -0400

    main.cpp: add img setting and preset for ppm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 int main(){
 
+    //Image
     
+    int image_height    = 256;
+    int image_width     = 256;
+    
+    //Render
+
+    std::cout << "P3\n" << image_height << " " << image_width << "\n255\n";
     return 0;
 }
